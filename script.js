158127e7095bc9fc8aef9a8558cec4d14f30a963
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-11-09 21:53:37 -0300

    função para o faq criada

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ function initTabNav() {
 }
 
 initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+
+  if (accordionList) {
+    accordionList[0].classList.add('ativo');
+    accordionList[0].nextElementSibling.classList.add('ativo');
+    function activeAccordion() {
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+}
+
+initAccordion();
