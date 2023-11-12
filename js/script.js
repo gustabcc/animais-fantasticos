12ad83777e5629e507b3a80f5952f9d35e2f3f19
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-11-12 14:55:59 -0300

    criação do modal realizada

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+import initAccordion from './modules/accordionFaq.js';
+import initTabNav from './modules/tabNav.js';
+import initModal from './modules/modal.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
--- script.js
+++
@@ -1,7 +0,0 @@
-import initScrollSuave from './scroll-suave.js';
-import initAnimacaoScroll from './animacao-scroll.js';
-import initAccordion from './accordionFaq.js';
-initScrollSuave();
-initAnimacaoScroll();
-initAccordion();
-initTabNav();
